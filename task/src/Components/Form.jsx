4e0ac6dca51c5718880fd7e7a2a1e73a7f2be8f5
Mayurwagh98 <mswagh1998@gmail.com>
2023-05-02 10:51:46 +0530

    Added: Address and Other details section

--- task/src/Components/Form.jsx
+++ task/src/Components/Form.jsx
@@ -3,13 +3,18 @@ import { useForm } from "react-hook-form";
 import axios from "axios";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import "./Form.css";
 
 const Form = () => {
   const schema = yup.object().shape({
     name: yup.string().required("Name is a Required Field!"),
     age: yup.number().positive().integer().required("Age is a Required Field!"),
     gender: yup.string().required("Gender is a Required Field!"),
-    mobile: yup.string().min(10).max(10).required(),
+    mobile: yup
+      .string()
+      .min(10)
+      .max(10)
+      .required("Mobile number should be of min 10 digits"),
     issueId: yup.string().required("ID Type is required"),
     govtId: yup
       .string()
@@ -26,6 +31,16 @@ const Form = () => {
     guardian_name: yup.string(),
     email: yup.string().email(),
     emergency_no: yup.string().min(10).max(10).required(),
+    address: yup.string(),
+    state: yup.string(),
+    city: yup.string(),
+    country: yup.string(),
+    pincode: yup.string(),
+    occupation: yup.string(),
+    region: yup.string(),
+    marital_status: yup.string(),
+    blood_group: yup.string(),
+    nationality: yup.string(),
   });
 
   const {
@@ -116,6 +131,76 @@ const Form = () => {
           />
         </section>
         {/* ---------------- Address Details ------------------ */}
+        <section>
+          <h3>Address Details</h3>
+          <label>Address</label>
+          <input
+            type="text"
+            placeholder="Enter your address"
+            {...register("address")}
+          />
+          <label>State</label>
+          <select {...register("state")}>
+            <option value="">Default</option>
+            <option value="maharashtra">Maharashtra</option>
+            <option value="assam">Assam</option>
+            <option value="bihar">Bihar</option>
+            <option value="gujrat">Gujrat</option>
+          </select>
+          <label>City</label>
+          <input
+            type="text"
+            placeholder="Enter your city"
+            {...register("city")}
+          />
+          <label>Country</label>
+          <input
+            type="text"
+            placeholder="Enter your country"
+            {...register("country")}
+          />
+          <label>Pincode</label>
+          <input
+            type="text"
+            placeholder="Enter pincode"
+            {...register("pincode")}
+          />
+        </section>
+        {/* -------------- Other Details ----------------------- */}
+        <section>
+          <h3>Other Details</h3>
+          <label>Occupation</label>
+          <input
+            type="text"
+            placeholder="Enter your occupation"
+            {...register("occupation")}
+          />
+          <label>Region</label>
+          <input
+            type="text"
+            placeholder="Enter your region"
+            {...register("region")}
+          />
+          <label>Marital Status</label>
+          <select id="martial_status" {...register("marital_status")}>
+            <option value="">Default</option>
+            <option value="single">Single</option>
+            <option value="married">Married</option>
+          </select>
+          <label>Blood Group</label>
+          <select id="blood_group" {...register("blood_group")}>
+            <option value="">Default</option>
+            <option value="A+">A+</option>
+            <option value="AB+">AB+</option>
+            <option value="O+">O+</option>
+          </select>
+          <label>Blood Group</label>
+          <input
+            type="text"
+            placeholder="Enter your nationality"
+            {...register("nationality")}
+          />
+        </section>
         <input type="submit" value="Submit" />
       </form>
     </div>
