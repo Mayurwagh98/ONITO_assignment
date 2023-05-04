6511a5a18d92250e099e9e6e09ac06c974f9bf16
Mayurwagh98 <mswagh1998@gmail.com>
2023-05-04 12:36:30 +0530

    Pushed: All updated files

--- task/src/pages/Dashboard.jsx
+++ task/src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import DataTable from "react-data-table-component";
 import { TableWithPagination } from "../Components/Table";
 
 const Dashboard = () => {
@@ -9,6 +8,7 @@ const Dashboard = () => {
   let getUsers = async () => {
     await axios
       .get(`http://localhost:8080/user?q=${search}`)
+      // .get(`https://onito-backend-jwvv.onrender.com/user?q=${search}`)
       .then((res) => {
         console.log(res.data);
 
@@ -21,54 +21,62 @@ const Dashboard = () => {
     getUsers();
   }, [search]);
 
-//   const columns = [
-//     {
-//       name: "ID",
-//       selector: (row) => row.id,
-//     },
-//     {
-//       name: "Name",
-//       selector: (row) => row.name,
-//       sortable: true,
-//     },
-//     {
-//       name: "Age",
-//       selector: (row) => row.age,
-//       sortable: true,
-//     },
-//     {
-//       name: "Mobile",
-//       selector: (row) => row.mobile,
-//       sortable: true,
-//     },
-//     {
-//       name: "Address",
-//       selector: (row) => row.address,
-//     },
-//     {
-//       name: "Govt ID",
-//       selector: (row) => row.govtId,
-//       sortable: true,
-//     },
-//     {
-//       name: "Gaurdian details",
-//       selector: (row) => row.guardian_name,
-//     },
-//     {
-//       name: "Nationality",
-//       selector: (row) => row.nationality,
-//     },
-//   ];
+  //   const columns = [
+  //     {
+  //       name: "ID",
+  //       selector: (row) => row.id,
+  //     },
+  //     {
+  //       name: "Name",
+  //       selector: (row) => row.name,
+  //       sortable: true,
+  //     },
+  //     {
+  //       name: "Age",
+  //       selector: (row) => row.age,
+  //       sortable: true,
+  //     },
+  //     {
+  //       name: "Mobile",
+  //       selector: (row) => row.mobile,
+  //       sortable: true,
+  //     },
+  //     {
+  //       name: "Address",
+  //       selector: (row) => row.address,
+  //     },
+  //     {
+  //       name: "Govt ID",
+  //       selector: (row) => row.govtId,
+  //       sortable: true,
+  //     },
+  //     {
+  //       name: "Gaurdian details",
+  //       selector: (row) => row.guardian_name,
+  //     },
+  //     {
+  //       name: "Nationality",
+  //       selector: (row) => row.nationality,
+  //     },
+  //   ];
 
   return (
     <div>
-      <h2>Dashboard</h2>
+      <h2 style={{ textDecoration: "underline", fontFamily: "sans-serif" }}>
+        Dashboard
+      </h2>
       <div>
         <input
           type="text"
           placeholder="Search........."
           onChange={(e) => setSearch(e.target.value)}
-          style={{ padding: "10px", width: "30%", borderRadius: "10px" }}
+          style={{
+            padding: "10px",
+            width: "30%",
+            borderRadius: "10px",
+            margin: "10px 0 0 0",
+            boxShadow: " rgba(0, 0, 0, 0.24) 0px 3px 8px",
+          }}
         />
       </div>
       {/* <DataTable columns={columns} data={users} pagination fixedHeader /> */}
