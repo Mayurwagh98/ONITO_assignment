25b89cb8037cf7e3ef36a32cba25e2ad6f39e8f1
Mayurwagh98 <mswagh1998@gmail.com>
2023-05-04 11:54:24 +0530

    Separated: schema

--- task/src/pages/Form.jsx
+++ task/src/pages/Form.jsx
@@ -1,62 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import "./Form.css";
 import { useNavigate } from "react-router-dom";
 import Modal from "@mui/material/Modal";
 import Alert from "@mui/material/Alert";
 import { ErrorAlert } from "../Components/ErrorAlert";
+import { schema } from "../Components/Schema";
 
 const Form = () => {
   let navigate = useNavigate();
   let [flag, setFlag] = useState(false);
   const [open, setOpen] = React.useState(false);
-
-  const schema = yup.object().shape({
-    name: yup.string().required("Name is a Required Field!"),
-    age: yup.string().required("Age is a Required Field!"),
-    gender: yup.string().required("Gender is a Required Field!"),
-    mobile: yup
-      .string()
-      .matches(/^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/, "Invalid mobile number")
-      .required(),
-    issueId: yup.string().required("ID Type is required"),
-    govtId: yup
-      .string()
-      .test("govtId", "Invalid government ID", function (value) {
-        const issueId = this.parent.issueId;
-        if (issueId === "aadhar") {
-          return /^\d{12}$/.test(value);
-        } else if (issueId === "pan") {
-          return /^[\w\d]{10}$/.test(value);
-        }
-        return true;
-      }),
-    guardian: yup.string(),
-    guardian_name: yup.string(),
-    email: yup.string().email(),
-    emergency_no: yup
-      .string()
-      .matches(
-        /^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/,
-        "Invalid emergency contact number"
-      )
-      .min(10)
-      .max(10)
-      .required(),
-    address: yup.string(),
-    state: yup.string(),
-    city: yup.string(),
-    country: yup.string(),
-    pincode: yup.string(),
-    occupation: yup.string(),
-    region: yup.string(),
-    marital_status: yup.string(),
-    blood_group: yup.string(),
-    nationality: yup.string(),
-  });
 
   const {
     register,
@@ -76,6 +32,7 @@ const Form = () => {
         setOpen(true);
         setTimeout(() => {
           setOpen(false);
+          navigate("/dashboard");
         }, 1400);
       })
       .catch((e) => {
@@ -100,7 +57,7 @@ const Form = () => {
           </Alert>
         </Modal>
       ) : null}
-      <ErrorAlert errors={errors} />
+      {errors && <ErrorAlert errors={errors} />}
 
       <form onSubmit={handleSubmit(handleRegister)}>
         {/* -------------- Personal Details ----------------- */}
